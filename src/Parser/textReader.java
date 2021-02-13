74170e0dc312826033fa27993149b14d242f511c
lenovo <lenovo@DESKTOP-FK8PAEL>
2021-02-13 13:13:09 -0800

    Text reader working on list

--- src/Parser/textReader.java
+++ src/Parser/textReader.java
@@ -2,12 +2,31 @@ package Parser;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner; // Import the Scanner class to read text files
+
 
 public class textReader {
 
 	public static void main(String[] args) throws IOException{
-
+		
+		 class Tuple<String, String1, Integer> {
+		    public Tuple(String1 string, String1 string2, int i) {
+				// TODO Auto-generated constructor stub
+		    	First=string;
+		    	Second= string2;
+		    	Third=i;
+			}
+			public String1 First;
+		    public String1 Second;
+		    public int Third;    
+		}
+		 
+		List<Tuple<String, String, Integer>> list = null;
+		 
+		list.add(new Tuple<String, String, Integer>("hello","jh", 1));
+		 
 		String arr[] = new String[6];
 		Scanner myReader = new Scanner(new FileReader("D:\\3221\\gg.txt"));
 		for (int i = 0; i < 6; i++) {
@@ -23,11 +42,11 @@ public class textReader {
 					if((getCharFromString(arr[j],i-1)=='-')&&(getCharFromString(arr[j],i+1)=='-')) {
 						System.out.print(getCharFromString(arr[j],0));
 						System.out.println(getCharFromString(arr[j],i));
+						//list.add(Character.toString(getCharFromString(arr[j],0)),Character.toString(getCharFromString(arr[j],i)),j);
 					}
 					//2 digits
 					else if((getCharFromString(arr[j],i-1)=='-')&&(getCharFromString(arr[j],i+1)!='-')&&(getCharFromString(arr[j],i+2)=='-')) {
 						System.out.print(getCharFromString(arr[j],0));
-
 						System.out.println(Character.toString(getCharFromString(arr[j],i))+Character.toString(getCharFromString(arr[j],i+1)));
 						i++;
 					}
@@ -35,8 +54,7 @@ public class textReader {
 					else if((getCharFromString(arr[j],i-1)=='-')&&(getCharFromString(arr[j],i+1)!='-')&&(getCharFromString(arr[j],i+2)!='-')&&(getCharFromString(arr[j],i+3)=='-')) {
 						System.out.print(getCharFromString(arr[j],0));
 						System.out.println(Character.toString(getCharFromString(arr[j],i))+Character.toString(getCharFromString(arr[j],i+1))+Character.toString(getCharFromString(arr[j],i+2)));
-						i++;
-						i++;
+						i+=2;
 					}
 				}
 			}
@@ -47,4 +65,6 @@ public class textReader {
 	public static char getCharFromString(String str, int index) {
 		return str.charAt(index);
 	}
+	
+	
 }
