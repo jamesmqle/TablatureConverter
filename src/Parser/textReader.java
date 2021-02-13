f62b11c29747fab9e126fc22e54be64893bfcb70
lenovo <lenovo@DESKTOP-FK8PAEL>
2021-02-13 12:37:40 -0800

    textReader 1

--- src/Parser/textReader.java
+++ src/Parser/textReader.java
@@ -1,83 +1,50 @@
 package Parser;
 
-import java.io.BufferedReader;
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner; // Import the Scanner class to read text files
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-
 public class textReader {
-	public static void main(String[] args) throws IOException {
-		Scanner in = new Scanner(new FileReader("C:\\Users\\James Le\\IdeaProjects\\TablatureConverter\\src\\sample\\hotCrossBunsGuitarTab"));
-		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\James Le\\IdeaProjects\\TablatureConverter\\src\\sample\\hotCrossBunsGuitarTab"));
-		
-		try {
-		    StringBuilder sb = new StringBuilder();
-		    String line = br.readLine();
 
-		    while (line != null) {
-		        sb.append(line);
-		        sb.append(System.lineSeparator());
-		        line = br.readLine();
-		    }
-		    String everything = sb.toString();
-		    System.out.println(everything);
+	public static void main(String[] args) throws IOException{
 
-		   // toVerticalWords(everything);
+		String arr[] = new String[6];
+		Scanner myReader = new Scanner(new FileReader("D:\\3221\\gg.txt"));
+		for (int i = 0; i < 6; i++) {
+			String data = myReader.nextLine();
+			System.out.println(data);
+			arr[i] = data;
+		}
+		myReader.close();
+		for(int i=2;i<60;i++) {
+			for(int j=0;j<6;j++) {
+				if((getCharFromString(arr[j],i)!='-')&&(getCharFromString(arr[j],i)!='|')) {
+					//1 digit
+					if((getCharFromString(arr[j],i-1)=='-')&&(getCharFromString(arr[j],i+1)=='-')) {
+						System.out.print(getCharFromString(arr[j],0));
+						System.out.println(getCharFromString(arr[j],i));
+					}
+					//2 digits
+					else if((getCharFromString(arr[j],i-1)=='-')&&(getCharFromString(arr[j],i+1)!='-')&&(getCharFromString(arr[j],i+2)=='-')) {
+						System.out.print(getCharFromString(arr[j],0));
 
-
-			Pattern pt = Pattern.compile("\\d");
-			Matcher mt = pt.matcher(everything);
-			while(mt.find()){
-				System.out.print(mt.group());
+						System.out.println(Character.toString(getCharFromString(arr[j],i))+Character.toString(getCharFromString(arr[j],i+1)));
+						i++;
+					}
+					//3 digits
+					else if((getCharFromString(arr[j],i-1)=='-')&&(getCharFromString(arr[j],i+1)!='-')&&(getCharFromString(arr[j],i+2)!='-')&&(getCharFromString(arr[j],i+3)=='-')) {
+						System.out.print(getCharFromString(arr[j],0));
+						System.out.println(Character.toString(getCharFromString(arr[j],i))+Character.toString(getCharFromString(arr[j],i+1))+Character.toString(getCharFromString(arr[j],i+2)));
+						i++;
+						i++;
+					}
+				}
 			}
-		} 
-		finally {
-		    br.close();
 		}
 
 	}
 
-	/*// This method will make the String vertical
-	private static void toVerticalWords(String str) {
-		//split the words by whitespace
-		String[] strArr = str.split("\\s");
-		int maxWordLen = 0;
-
-		//get the longest word length
-		for(String strTemp : strArr) {
-			if(strTemp.length() > maxWordLen)
-				maxWordLen = strTemp.length();
-		}
-
-		//make a matrix of the words with each character in an array block
-		char[][] charArr = new char[strArr.length][maxWordLen];
-		for(int i=0; i<strArr.length; i++) {
-			int j=0;
-			for(char ch : strArr[i].toCharArray()){
-				charArr[i][j] = ch;
-				j++;
-			}
-		}
-
-		//print the vertical word pattern, or transpose of above matrix (2D array)
-		for(int j=0; j<maxWordLen; j++) {
-			for(int i=0; i<strArr.length; i++) {
-				if (i!=0)
-					System.out.print(" ");
-				System.out.print(charArr[i][j]);
-			}
-
-			System.out.println();
-		}
-	}*/
-
-
-
-
-
+	public static char getCharFromString(String str, int index) {
+		return str.charAt(index);
+	}
 }
