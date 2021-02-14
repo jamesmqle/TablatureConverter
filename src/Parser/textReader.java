60eaef723d3076c4fffab6027735fdf0548aa9e4
lenovo <lenovo@DESKTOP-FK8PAEL>
2021-02-14 01:27:08 -0800

    text reader with output

--- src/Parser/textReader.java
+++ src/Parser/textReader.java
@@ -7,26 +7,19 @@ import java.util.List;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 
-public class textReader {
+public class textReader extends Output{
+
+	public textReader(String string, String string2, int i) {
+		super(string, string2, i);
+		// TODO Auto-generated constructor stub
+	}
+
 
 	public static void main(String[] args) throws IOException{
 		
-		 class Tuple<String, String1, Integer> {
-		    public Tuple(String1 string, String1 string2, int i) {
-				// TODO Auto-generated constructor stub
-		    	First=string;
-		    	Second= string2;
-		    	Third=i;
-			}
-			public String1 First;
-		    public String1 Second;
-		    public int Third;    
-		}
-		 
-		List<Tuple<String, String, Integer>> list = null;
-		 
-		list.add(new Tuple<String, String, Integer>("hello","jh", 1));
-		 
+		 List<Output> dataa= new ArrayList<>();
+		 dataa.add(new Output("A","s12",2));
+		
 		String arr[] = new String[6];
 		Scanner myReader = new Scanner(new FileReader("D:\\3221\\gg.txt"));
 		for (int i = 0; i < 6; i++) {
@@ -59,12 +52,17 @@ public class textReader {
 				}
 			}
 		}
-
+		printData(dataa);
 	}
+
 
 	public static char getCharFromString(String str, int index) {
 		return str.charAt(index);
 	}
 	
+	public static void printData(List<Output> data)
+	{
+	   data.forEach(obj -> System.out.println("item1" + obj.getletter() + " " + "item2" + obj.getnum() + " " + "item2" + obj.getindex()));
+	}
 	
 }
