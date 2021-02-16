99290c9111dbcbe05d40746eb4a7c1ed22bf92b3
Temiloluwa Odunfa <odunifa.temi@gmail.com>
2021-02-16 18:20:37 -0500

    debugging

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -8,10 +8,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    public static void main(String[] args) {
+	
+	public static void main(String[] args) {
         launch(args);
-    }
+	}
 
     @Override
     public void start(Stage primaryStage) throws Exception{
