b7da97adf8b7dd4d2ce1c59c7332b953daa51488
lenovo <lenovo@DESKTOP-FK8PAEL>
2021-02-28 11:58:41 -0800

    Start testing textReader.java and Output.java

---
+++ src/Parser/textReaderTest.java
@@ -0,0 +1,84 @@
+package Parser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class textReaderTest {
+
+	@Test
+	public void testTextReader() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadTabFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParsGuitar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParsBass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParsDrum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCharFromString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOutput() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetindex() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetnote1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetnote2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetletter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGettech() {
+		fail("Not yet implemented");
+	}
+
+}
