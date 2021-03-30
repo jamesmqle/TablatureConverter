1b432f81dc3207ff059c9ce652593ceeadbf458e
jamesmqle <jamesmqle01@gmail.com>
2021-03-29 22:58:02 -0400

    restructured parser + fixed technical - everyone

---
+++ src/main/java/Parser/BassParser.java
@@ -0,0 +1,102 @@
+package Parser;
+
+import java.util.List;
+
+import static Parser.textReader.getCharFromString;
+import static Parser.textReader.isInteger;
+
+public class BassParser {
+
+    /*
+     * "ParsBass" Takes the tab to parse and the list of the results so far. Then
+     * makes changes and adds new elements to the list. returns the result list of
+     * Output.
+     */
+
+    public static List<Output> ParseBass(List<String> tab, List<Output> list) {
+        // Check if the list is empty to add new tab element to the list
+        if (!list.isEmpty()) {
+            list.add(new Output("# NEW TAB #", -2, -2, "-", -2));
+        }
+        int length = tab.get(0).length();
+        for (int i = 2; i < length; i++) {
+            for (int j = 0; j < 4; j++) {
+                if ((getCharFromString(tab.get(j), i) != '-') && (getCharFromString(tab.get(j), i) != '|')) {
+                    // 1 digit
+                    // Check if the note is proper 1 digit to add new tab element to the list
+                    if (((getCharFromString(tab.get(j), i - 1) == '-')
+                            || (getCharFromString(tab.get(j), i - 1) == '|'))
+                            && ((getCharFromString(tab.get(j), i + 1) == '-')
+                            || (getCharFromString(tab.get(j), i + 1) == '|'))) {
+                        if (isInteger(Character.toString(getCharFromString(tab.get(j), i)))) {
+                            list.add(new Output(Character.toString(getCharFromString(tab.get(j), 0)),
+                                    Integer.parseInt(Character.toString(getCharFromString(tab.get(j), i))), -1, "-",
+                                    i));
+                        } else {
+                            list.add(new Output(Character.toString(getCharFromString(tab.get(j), 0)), -1, -1,
+                                    Character.toString(getCharFromString(tab.get(j), i)), i));
+                        }
+
+                    }
+                    // 2 digits
+                    // Check if the note is proper 2 digit to add new tab element to the list
+                    else if (((getCharFromString(tab.get(j), i - 1) == '-')
+                            || (getCharFromString(tab.get(j), i - 1) == '|'))
+                            && ((getCharFromString(tab.get(j), i + 2) == '-')
+                            || (getCharFromString(tab.get(j), i + 2) == '|'))
+                            && ((getCharFromString(tab.get(j), i + 1) != '-')
+                            || (getCharFromString(tab.get(j), i + 1) != '|'))) {
+                        list.add(
+                                new Output(Character.toString(getCharFromString(tab.get(j), 0)),
+                                        Integer.parseInt(Character.toString(getCharFromString(tab.get(j), i))
+                                                + Character.toString(getCharFromString(tab.get(j), i + 1))),
+                                        -1, "-", i));
+
+                    }
+                    // 3 digits number in parentheses like (0)
+                    // Check if the note is proper 3 digit to add new tab element to the list
+                    else if ((getCharFromString(tab.get(j), i - 1) == '-')
+                            && (getCharFromString(tab.get(j), i + 1) != '-')
+                            && (getCharFromString(tab.get(j), i + 1) != '/')
+                            && (getCharFromString(tab.get(j), i + 1) != 's')
+                            && (getCharFromString(tab.get(j), i + 2) != '-')
+                            && (getCharFromString(tab.get(j), i + 3) == '-')) {
+                        list.add(new Output(Character.toString(getCharFromString(tab.get(j), 0)),
+                                Integer.parseInt(Character.toString(getCharFromString(tab.get(j), i))
+                                        + Character.toString(getCharFromString(tab.get(j), i + 1))
+                                        + Character.toString(getCharFromString(tab.get(j), i + 2))),
+                                -1, "-", i));
+                    }
+
+                    // 3 digit with technique
+                    else if (((getCharFromString(tab.get(j), i - 1) == '-')
+                            || (getCharFromString(tab.get(j), i - 1) == '|'))
+                            && ((getCharFromString(tab.get(j), i + 1) == '/')
+                            || (getCharFromString(tab.get(j), i + 1) == 's')
+                            || (getCharFromString(tab.get(j), i + 1) == 'p')
+                            || (getCharFromString(tab.get(j), i + 1) == 'h'))
+                            && ((getCharFromString(tab.get(j), i + 2) != '-')
+                            || (getCharFromString(tab.get(j), i + 2) != '|'))
+                            && ((getCharFromString(tab.get(j), i + 3) == '-')
+                            || (getCharFromString(tab.get(j), i + 3) == '|'))) {
+                        list.add(new Output(Character.toString(getCharFromString(tab.get(j), 0)),
+                                Integer.parseInt(Character.toString(getCharFromString(tab.get(j), i))),
+                                Integer.parseInt(Character.toString(getCharFromString(tab.get(j), i + 2))),
+                                Character.toString(getCharFromString(tab.get(j), i + 1)), i));
+                    }
+                }
+                // Check if the element is "|" to add new tab element to the list
+                if (getCharFromString(tab.get(j), i) == '|') {
+                    list.add(new Output("*New Measure*", -1, -1, "-", i));
+                    if (i != length - 1) {
+                        i++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return list;
+    }
+
+}
