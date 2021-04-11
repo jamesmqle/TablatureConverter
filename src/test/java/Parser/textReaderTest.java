5dd00422dae9406edcac615264e49e52e99708fd
Amin Mohammadi <amin45mh@gmail.com>
2021-04-11 04:02:41 -0400

    textreader test

--- src/test/java/Parser/textReaderTest.java
+++ src/test/java/Parser/textReaderTest.java
@@ -26,20 +26,19 @@ class textReaderTest {
         assertTrue(listt.get(0).getnote1()==0);
     }
 
-//    @Test
-//    public void testTabIsOK_guitar_1() {
-//        assertThrows(StringIndexOutOfBoundsException.class, () -> {
-//            int flag=1;         //Its guitar
-//            List<String> guitartab = new ArrayList<>();
-//            guitartab.add("E|--0-----------------------|-------------------------|");
-//            guitartab.add("B|------------------3-----5-|-2----------------------|");
-//            guitartab.add("G|------------------3-------|-2-----------------------|");
-//            guitartab.add("D|------------------5-------|-2-----------------------|");
-//            guitartab.add("A|--------------------------|-0-----------------------|");
-//            guitartab.add("D|--------------------------|-------------------------|");
-//            textReader.TabIsOK(guitartab, flag);
-//        });
-//    }
+    @Test
+    public void testTabIsOK_guitar_1() {
+        int[] arr = new int[3];
+        List<String> guitartab = new ArrayList<>();
+        guitartab.add("E|--0-----------------------|-------------------------|");
+        guitartab.add("B|------------------3-----5-|-2----------------------|");
+        guitartab.add("G|------------------3-------|-2-----------------------|");
+        guitartab.add("D|------------------5-------|-2-----------------------|");
+        guitartab.add("A|--------------------------|-0-----------------------|");
+        guitartab.add("D|--------------------------|-------------------------|");
+        arr=TabIsOKTracker(guitartab, 1);
+        assertEquals(-1, arr[1]);
+    }
 //
 //    @Test
 //    public void testTabIsOK_guitar_2() {
@@ -69,19 +68,18 @@ class textReaderTest {
         assertTrue(listt.get(1).getnote1()==55);
     }
 
-//    @Test
-//    public void testTabIsOK_bass_1() {
-//        assertThrows(StringIndexOutOfBoundsException.class, () -> {
-//            int flag=2;         //Its bass
-//            List<String> basstab = new ArrayList<>();
-//            basstab.add("G|-----------------------------------------------|");
-//            basstab.add("D|-----------------------------------------------|");
-//            basstab.add("A|-5-55-5-5-0--4-44-4-4-0--2-22-2-2-0-----------2|");
-//            basstab.add("E|------------------------------------3-33-3-5--|");
-//            textReader.TabIsOK(basstab, flag);
-//        });
-//    }
-//
+    @Test
+    public void testTabIsOK_bass_1() {
+        int[] arr = new int[3];
+        List<String> basstab = new ArrayList<>();
+        basstab.add("G|-----------------------------------------------|");
+        basstab.add("D|-----------------------------------------------|");
+        basstab.add("A|-5-55-5-5-0--4-44-4-4-0--2-22-2-2-0-----------2|");
+        basstab.add("E|------------------------------------3-33-3-5--|");
+        arr=TabIsOKTracker(basstab, 2);
+        assertEquals(-1, arr[1]);
+    }
+
 //    @Test
 //    public void testTabIsOK_bass_2() {
 //        assertThrows(StringIndexOutOfBoundsException.class, () -> {
@@ -111,37 +109,35 @@ class textReaderTest {
         assertTrue(listt.get(1).gettech().charAt(0)=='o');
     }
 
-//    @Test
-//    public void testTabIsOK_drum_1() {
-//        assertThrows(StringIndexOutOfBoundsException.class, () -> {
-//            int flag=3;         //Its drum
-//            List<String> drumtab = new ArrayList<>();
-//            drumtab.add("CC|x---------------|--------x-------|");
-//            drumtab.add("HH|--x-x-x-x-x-x-x-|---------------|");
-//            drumtab.add("SD|----o-------o---|oooo------------|");
-//            drumtab.add("HT|----------------|----oo----------|");
-//            drumtab.add("MT|----------------|------oo--------|");
-//            drumtab.add("BD|o-------o-------|o-------o-------|");
-//            textReader.TabIsOK(drumtab, flag);
-//        });
-//    }
-
-    /*
     @Test
-    public void testTabIsOK_drum_2() {
-        assertThrows(StringIndexOutOfBoundsException.class, () -> {
-            int flag=3;         //Its drum
-            List<String> drumtab = new ArrayList<>();
-            drumtab.add("CC|x---------------|--------x-------|");
-            drumtab.add("HH|--x-x-x-x-x-x-x-|----------------|");
-            drumtab.add("SD|----o-------o---|oooo------------|");
-            drumtab.add("HT|----------------|----oo----------|");
-            drumtab.add("MT|----------------|------oo--------|");
-            drumtab.add("BD|o-------o-------|o-------o-------|");
-            textReader.TabIsOK(drumtab, flag);
-        });
+    public void testTabIsOK_drum_1() {
+        int[] arr = new int[3];
+        List<String> drumtab = new ArrayList<>();
+        drumtab.add("CC|x---------------|--------x-------|");
+        drumtab.add("HH|--x-x-x-x-x-x-x-|--------------|");
+        drumtab.add("SD|----o-------o---|oooo------------|");
+        drumtab.add("HT|----------------|----oo----------|");
+        drumtab.add("MT|----------------|------oo--------|");
+        drumtab.add("BD|o-------o-------|o-------o-------|");
+        arr=TabIsOKTracker(drumtab, 3);
+        assertEquals(-1, arr[1]);
     }
-    */
+
+    @Test
+    public void testTabIsOK_unknown() {
+        int[] arr = new int[3];
+        List<String> tab = new ArrayList<>();
+        tab.add("C|p---------------|--------1-------|");
+        arr=TabIsOKTracker(tab, 4);
+        assertEquals(10, arr[0]);
+    }
+
+    @Test
+    public void testIsInteger() {
+        String s = "2";
+        boolean b = isInteger(s);
+        assertTrue(b==true);
+    }
 
     @Test
     public void testOutput() {
@@ -183,4 +179,6 @@ class textReaderTest {
         Output o = new Output("e", 2, 3, "4", 1);
         assertEquals("4", o.gettech());
     }
+
+
 }
